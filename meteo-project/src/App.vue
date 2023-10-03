3dd3242d3861765adf1377275f317507888a8ee1
[Bastien] <[bastien.pelletier@le-campus-numerique.fr]>
2023-10-03 11:28:09 +0200

    Ville 1 added on City components

--- meteo-project/src/App.vue
+++ meteo-project/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
     <OnListClick/>
-    <CitiesList/>
+<!--    <CitiesList/>-->
+    <router-view></router-view>
+    <router-link to="/cities"></router-link>
+    <router-link to="/ville"></router-link>
+
   </div>
 </template>
 
 <script>
 import OnListClick from "./components/OnListClick.vue";
 import CitiesList from "./views/CitiesList.vue";
+import City from "./components/City.vue";
 
 export default {
   components: {
     CitiesList,
     OnListClick,
+    City
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
---
+++ meteo-project/src/components/City.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p> {{name}} </p>
+    <p> {{weather}} </p>
+    <p> {{temperature}} </p>
+    <p> {{updatedAt.toLocaleString()}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Ville 1',
+      weather: 'Pluie diluvienne',
+      temperature: 15,
+      updatedAt: new Date()
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
